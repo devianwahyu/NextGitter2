9addfa3dae7216824d8c21f14450dc241f44aea8
devianwahyu <devianwahyu666@gmail.com>
2021-03-21 12:23:09 +0700

    add splashscreen

---
+++ app/src/main/java/com/example/nextgitter2/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.nextgitter2.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.nextgitter2.MainActivity
+import com.example.nextgitter2.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
